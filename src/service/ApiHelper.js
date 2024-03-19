392e4b4aa8beac9a462ca1ab2ebddd8114320982
Jess Eliza Paul <jess@qubicle.co.in>
2024-03-19 14:43:07 +0530

    multipart header added

--- src/service/ApiHelper.js
+++ src/service/ApiHelper.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-
 import CryptoJS from 'crypto-js';
 
 const BASE_URL = `${process.env.REACT_APP_API_BASE_URL}`;
@@ -10,18 +9,18 @@ const axiosInstance = axios.create({
 });
 
 axiosInstance.interceptors.request.use((config) => {
-  const sttoken= localStorage.getItem('token');
-  const secretPass = "j123@nglez678$one";
+  const sttoken = localStorage.getItem('token');
+  const secretPass = 'j123@nglez678$one';
   const detoken = CryptoJS.AES.decrypt(sttoken, secretPass).toString(CryptoJS.enc.Utf8);
   const token = JSON.parse(detoken);
   const headers = {
     Authorization: `Bearer ${token}`,
+    'Content-Type': 'multipart/form-data',
   };
   config.params = config.params || {};
   config.headers = headers;
 
   return config;
 });
-
 
 export default axiosInstance;

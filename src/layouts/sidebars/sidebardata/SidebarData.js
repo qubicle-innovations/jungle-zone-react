c67c835b8db95ff6a975796f0903ac55aa9f6a81
Jess Eliza Paul <jess@qubicle.co.in>
2024-03-27 13:46:05 +0530

    new menus added

--- src/layouts/sidebars/sidebardata/SidebarData.js
+++ src/layouts/sidebars/sidebardata/SidebarData.js
@@ -9,26 +9,63 @@ const SidebarData = [
     collapisble: false,
   },
   {
+    title: 'Sub Admins',
+    href: '/home',
+    icon: <Icon.Users />,
+    id: 2,
+    collapisble: false,
+  },
+  {
     title: 'Coupon',
     href: '/coupon',
     icon: <Icon.CreditCard />,
-    id: 2,
+    id: 3,
     collapisble: false,
   },
   {
     title: 'Gallery',
     href: '/gallery',
     icon: <Icon.Image />,
-    id: 3,
+    id: 4,
     collapisble: false,
   },
   {
     title: 'Promotion',
     href: '/promotion',
     icon: <Icon.Volume2 />,
-    id: 4,
+    id: 5,
     collapisble: false,
   },
+  {
+    title: 'Attractions',
+    href: '/dashboards',
+    id: 6,
+    icon: <Icon.Grid />,
+    collapisble: true,
+    children: [
+      {
+        title: 'Catergories',
+        href: '/dashboards/minimal',
+        icon: <Icon.Disc />,
+        id: 6.1,
+        collapisble: false,
+      },
+      {
+        title: 'Sub-catergories',
+        href: '/dashboards/minimal',
+        icon: <Icon.Disc />,
+        id: 6.2,
+        collapisble: false,
+      },
+      {
+        title: 'Details',
+        href: '/dashboards/minimal',
+        icon: <Icon.Disc />,
+        id: 6.3,
+        collapisble: false,
+      },
+    ]
+  }
 ];
 
 export default SidebarData;
